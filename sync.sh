aa52460a2ac975e7e9bee51af81acc0461ad7ee4
Anish Nair <anishn@gmail.com>
2010-11-02 16:39:45 -0700

    Updates

--- sync.sh
+++
@@ -1,4 +0,0 @@
-rsync -avz --exclude '.git' ~anishn/s4-website/ soundfigure.greatamerica:/var/www/s4
-rsync -avz --exclude '.git' ~anishn/s4-website/css ~anishn/s4-website/images s4_s4@ssh.phx.nearlyfreespeech.net:/home/public/
-rsync -avz --exclude '.git' ~anishn/s4-website/pmwiki.tmpl s4_s4wiki@ssh.phx.nearlyfreespeech.net:/home/public/pmwiki/pub/skins/pmwiki
-rsync -avz --exclude '.git' ~anishn/s4-website/pmwiki.htaccess s4_s4wiki@ssh.phx.nearlyfreespeech.net:/home/public/.htaccess
