62437cbae3e04c0969fe985e263e10b4782a9db4
Anish Nair <anishn@gmail.com>
2010-11-05 19:00:06 -0700

    Misc changes

--- index.php
+++ index.php
@@ -9,8 +9,9 @@ if(isset($_GET['p'])) {
 $nav_bar = array(
 	"home" => "./",
 	"docs" => "http://wiki.s4.io",
-	"downloads" => "http://github.com/s4",
+	"code" => "http://github.com/s4",
 	"get involved" => "?p=contrib",
+	"blog" => "http://blog.s4.io",
 	"team" => "?p=team",
 #	"demo" => "?p=demo",
 	)
@@ -33,6 +34,23 @@ function email($u, $d, $l) {
 		        document.write(label + '</a>');
 		  // -->
 		  </script>"; 
+}
+
+function render_google_analytics () {
+	echo "<script type='text/javascript'>
+
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-19490961-1']);
+	  _gaq.push(['_setDomainName', '.s4.io']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+
+	</script>";
 }
 
 ?>
